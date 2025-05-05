bcab360588e21979d7b5cc7e7f41ac3374bab058
DeraCodings <otuonyechidera@yahoo.com>
2025-05-05 19:22:54 +0100

    updated the velt api key variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import EmailEditor from "./components/EmailEditor";
 
 export default function App() {
   return (
-    <VeltProvider apiKey="GCFFpYOSrrRXNLk9HxR7">
+    <VeltProvider apiKey={process.env.NEXT_PUBLIC_VELT_API_KEY || ""}>
       <div className="h-screen bg-slate-50">
         <YourAuthComponent />
         <EmailEditor />
