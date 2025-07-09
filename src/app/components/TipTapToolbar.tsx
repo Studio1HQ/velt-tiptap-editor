232a4bdd08f1cb1984036b540a51e5f28efd5b2e
DeraCodings <otuonyechidera@yahoo.com>
2025-07-09 18:23:35 +0100

    added the notifcation tool

--- src/app/components/TipTapToolbar.tsx
+++ src/app/components/TipTapToolbar.tsx
@@ -23,8 +23,10 @@ import {
   List,
   ListOrdered,
   Paperclip,
+  BellRing,
 } from "lucide-react";
 import { triggerAddComment } from "@veltdev/tiptap-velt-comments";
+import { VeltNotificationsTool } from "@veltdev/react";
 
 interface EditorToolbarProps {
   editor: Editor | null;
@@ -237,6 +239,11 @@ export default function EditorToolbar({ editor }: EditorToolbarProps) {
           <span>Comment</span>
         </Button>
       </div>
+      <Separator orientation="vertical" className="mx-1 h-6" />
+      {/* <Button className="hover:bg-gray-500 cursor-pointer duration-200">
+        <BellRing size={15} color="#fff" />
+      </Button> */}
+      <VeltNotificationsTool />
     </div>
   );
 }
